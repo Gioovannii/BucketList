b6ccc456976375651469d5b30af7d340407874b3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-24 10:04:25 +0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Giovanni Gaffé on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
