8f4c8240979d350a1240451ded95f2995caf7e39
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-29 10:35:29 +0800

    set a showing state edit

--- BucketList/Lecons/Map/MapContentView.swift
+++ BucketList/Lecons/Map/MapContentView.swift
@@ -13,6 +13,7 @@ struct MapContentView: View {
     @State private var locations = [MKPointAnnotation]()
     @State private var selectedPlace : MKPointAnnotation?
     @State private var showingPlaceDetails = false
+    @State private var showingEditScreen = false
     
     var body: some View {
         ZStack {
@@ -33,6 +34,9 @@ struct MapContentView: View {
                         newLocation.title = "Example location"
                         newLocation.coordinate = self.centerCoordinate
                         self.locations.append(newLocation)
+                        
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
                     }) {
                         Image(systemName: "plus")
                     }
@@ -47,8 +51,13 @@ struct MapContentView: View {
         }
         .alert(isPresented: $showingPlaceDetails) {
             Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                // edit this place
+                self.showingEditScreen = true
             })
+        }
+        .sheet(isPresented: $showingEditScreen) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
         }
         
         // MARK: - Simple map
