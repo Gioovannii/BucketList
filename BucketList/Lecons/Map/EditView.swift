250ed401e7867cdab551f009b9150d46cfc7ea4d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-29 10:51:31 +0800

    use loading state to show UI

--- BucketList/Lecons/Map/EditView.swift
+++ BucketList/Lecons/Map/EditView.swift
@@ -9,9 +9,15 @@ import MapKit
 import SwiftUI
 
 struct EditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
     
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark: MKPointAnnotation
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
     
     var body: some View {
         NavigationView {
@@ -21,12 +27,37 @@ struct EditView: View {
                     TextField("Description", text: $placemark.wrappedSubtitle)
 
                 }
+                
+                Section(header: Text("Nearby")) {
+                    if loadingState == .loaded {
+                        List(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    } else if loadingState == .loading {
+                        Text("Loading...")
+                    } else {
+                        Text("Please try again later.")
+                    }
+                }
             }
             .navigationBarTitle("Edit place")
             .navigationBarItems(trailing: Button("Done") {
                 self.presentationMode.wrappedValue.dismiss()
             })
             
+        }
+    }
+    
+    func nearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("bad URL: \(urlString)")
+            return
         }
     }
 }
